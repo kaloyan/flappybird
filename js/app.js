068475b6dc28a5e32dc5c28a2c300d1ccf57bb50
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-04-11 17:26:57 +0300

    Created app.js
    Created main game event loop

---
+++ js/app.js
@@ -0,0 +1,46 @@
+import { updateBird, setupBird, getBirdRect } from "./bird.js";
+
+document.addEventListener("keypress", handleStart, { once: true });
+
+const title = document.querySelector("[data-title]");
+const subtitle = document.querySelector("[data-subtitle]");
+
+let lastTime;
+
+function handleStart() {
+  title.classList.add("hide");
+
+  setupBird();
+  window.requestAnimationFrame(updateLoop);
+}
+
+function handleLose() {
+  title.classList.remove("hide");
+  subtitle.classList.remove("hide");
+  subtitle.textContent = "0 Pipes";
+
+  document.addEventListener("keypress", handleStart, { once: true });
+}
+
+function updateLoop(time) {
+  if (lastTime == null) {
+    lastTime = time;
+    window.requestAnimationFrame(updateLoop);
+    return;
+  }
+
+  const delta = time - lastTime;
+
+  updateBird(delta);
+  if (checkLose()) return handleLose();
+
+  lastTime = time;
+  window.requestAnimationFrame(updateLoop);
+}
+
+function checkLose() {
+  const birdRect = getBirdRect();
+
+  const outsideWorld = birdRect.top < 0 || birdRect.bottom > window.innerHeight;
+  return outsideWorld;
+}
